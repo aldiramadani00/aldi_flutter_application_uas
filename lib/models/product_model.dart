fa51cc337016c0a24c1c4e0c3c48a3f9206d9184
aldiramadani00 <alditipex123@gmail.com>
2023-07-31 17:16:47 +0700

    Anyar

--- lib/models/product_model.dart
+++ lib/models/product_model.dart
@@ -11,8 +11,20 @@ class Product {
   });
 
   static const List<Product> products = [
-    Product(name: 'Apple', price: 2.99, imageUrl: ''),
-    Product(name: 'Orange', price: 2.99, imageUrl: ''),
-    Product(name: 'Pear', price: 2.99, imageUrl: ''),
+    Product(
+        name: 'Kebab',
+        price: 2.99,
+        imageUrl:
+            'https://akcdn.detik.net.id/visual/2020/07/15/888366454-1_11.jpeg?w=720&q=90'),
+    Product(
+        name: 'Burger',
+        price: 2.99,
+        imageUrl:
+            'https://images.unsplash.com/photo-1603064752734-4c48eff53d05?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8NHx8QnVyZ2VyfGVufDB8fDB8fHww&auto=format&fit=crop&w=500&q=60'),
+    Product(
+        name: 'Kentang',
+        price: 2.99,
+        imageUrl:
+            'https://images.unsplash.com/photo-1541592106381-b31e9677c0e5?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8NXx8ZnJlbmNoJTIwZnJpZXN8ZW58MHx8MHx8fDA%3D&auto=format&fit=crop&w=500&q=60'),
   ];
 }
